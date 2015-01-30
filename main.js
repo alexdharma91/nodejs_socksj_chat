40f112a493aa3ae37abb0921f1b630a1db9e81c1
a.zavodin <a.zavodin@tcsbank.ru>
2015-01-30 18:14:48 +0300

    + deleteEntity and saveEntity methods

--- main.js
+++ main.js
@@ -13,67 +13,69 @@ var sequelize = new Sequelize('test', 'test', config.password, {
 });
 
 var Message = models.getMessageModel(sequelize, Sequelize);
-
-/*for(var i = 0; i <= 6; i++){
-    Message.sync({ force: true }).then(function () {return Message.create({content : 'test', sender : 'sender', reciever : 'bnmbnm', type : 'public'});});
-}*/
-
-
-/*
-Message.find(2).then(function (photo) {
-   console.log('photo = ' + photo);
-}).catch(function (error) {
-        res.send(500, error);
-});*/
-
 classMap['message'] = Message;
 
 var User = models.getUserModel(sequelize, Sequelize);
-
 classMap['user'] = User;
 
-for(var i = 0; i <= 15; i++){
-  //  var user = createUser('name' + i, 'surname' + i)
-
-   /* var user = User.build({name: 'name', surname : 'surnameParam', age : i});
-     var user = createUser('name' + i, 'surname' + i, i);*/
-
-//    var getObject = new Object();
-  //  saveEntity(getObject);
-}
-
-
-//Message.find({}).then(function(res){console.log(res)});
-
-
-exports.createUser = function createUser(nameParam, surnameParam, age){
-    var user = User.build({name: nameParam, surname : surnameParam, age : age});
-    User.sync({ force: false }).then(function () {return User.create(user.dataValues);});
+exports.createUser = function createUser(nameParam, surnameParam, age) {
+    var user = User.build({name: nameParam, surname: surnameParam, age: age});
+    User.sync({ force: false }).then(function () {
+        return User.create(user.dataValues);
+    });
     return user;
 };
 
-function saveEntity(entity){
-
-    if(entity.getMappedClass == undefined){
+exports.saveEntity = function (entity) {
+    if (entity.getMappedClass == undefined) {
         return;
     }
+    var persistclass = classMap[entity.getMappedClass()];
 
-   var persistclass = classMap[mappedClass];
+    if (entity.isNewRecord) {
+        persistclass.sync({ force: false }).then(function () {
+            return persistclass.create(entity.dataValues);
+        });
+    } else {
+        entity.updateAttributes(entity.dataValues)
+    }
 
-   if(entity.isNewRecord()){
-    persistclass.sync({ force: true }).then(function () {return persistclass.create(entity.dataValues);});
-   }else{
-    entity.updateAttributes(entity.dataValues)
-   }
-
-   var point = 80;
 }
 
-function putPrivateMessage(contentParam, senderIdParam, recieverIdParam){
-    Message.sync({ force: true }).then(function () {return User.create({ content: contentParam, sender : senderIdParam, reciever : recieverIdParam, type : 'private'});});
+exports.deleteEntity = function (entityClass, id) {
+    if (entityClass == undefined) {return;}
+    var persistclass = classMap[entityClass];
+    persistclass.destroy({where : {id:[id]}});
+};
+
+/*
+var options = {where : {id:[1,2,3]}}
+User.destroy(options);
+*/
+
+for(var i = 0; i < 10; i++){
+    exports.deleteEntity('user', i);
 }
 
-function putPublicMessage(content, senderId, recieverGroupId){
+
+
+/*
+сохранение
+for(var i = 0; i < 10; i++){
+    var user = User.build({login : 'login', password : 'password', age : 30});
+    exports.saveEntity(user);
+}
+*/
+
+
+
+function putPrivateMessage(contentParam, senderIdParam, recieverIdParam) {
+    Message.sync({ force: true }).then(function () {
+        return User.create({ content: contentParam, sender: senderIdParam, reciever: recieverIdParam, type: 'private'});
+    });
+}
+
+function putPublicMessage(content, senderId, recieverGroupId) {
 
 }
 
