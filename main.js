87829e25a8f4557a8ec5898f2d9a7cdc09b41bef
a.zavodin <a.zavodin@tcsbank.ru>
2015-01-30 18:51:03 +0300

    + findEntity method

--- main.js
+++ main.js
@@ -18,14 +18,7 @@ classMap['message'] = Message;
 var User = models.getUserModel(sequelize, Sequelize);
 classMap['user'] = User;
 
-exports.createUser = function createUser(nameParam, surnameParam, age) {
-    var user = User.build({name: nameParam, surname: surnameParam, age: age});
-    User.sync({ force: false }).then(function () {
-        return User.create(user.dataValues);
-    });
-    return user;
-};
-
+//  Общее сохранение
 exports.saveEntity = function (entity) {
     if (entity.getMappedClass == undefined) {
         return;
@@ -42,45 +35,17 @@ exports.saveEntity = function (entity) {
 
 }
 
+// Общее удаление
 exports.deleteEntity = function (entityClass, id) {
     if (entityClass == undefined) {return;}
     var persistclass = classMap[entityClass];
     persistclass.destroy({where : {id:[id]}});
 };
 
-/*
-var options = {where : {id:[1,2,3]}}
-User.destroy(options);
-*/
-
-for(var i = 0; i < 10; i++){
-    exports.deleteEntity('user', i);
+exports.findEntity = function(entityClass, id){
+    if (entityClass == undefined) {return;}
+    var persistclass = classMap[entityClass];
+    persistclass.find(id).success(function(entity){
+    console.log('finded');
+    })
 }
-
-
-
-/*
-сохранение
-for(var i = 0; i < 10; i++){
-    var user = User.build({login : 'login', password : 'password', age : 30});
-    exports.saveEntity(user);
-}
-*/
-
-
-
-function putPrivateMessage(contentParam, senderIdParam, recieverIdParam) {
-    Message.sync({ force: true }).then(function () {
-        return User.create({ content: contentParam, sender: senderIdParam, reciever: recieverIdParam, type: 'private'});
-    });
-}
-
-function putPublicMessage(content, senderId, recieverGroupId) {
-
-}
-
-
-
-
-
-
